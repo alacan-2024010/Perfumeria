0db14121f4d80bc9b6fa0ff1a0f83c73ad6787bb
pcalderon-2021547 <pcalderon-2021547@kinal.edu.gt>
2025-07-07 18:22:01 -0600

    Feat: Agerga la septima entidad con su respectivo CRUD

--- PerfumeriaYAccesoriosDB.sql
+++ PerfumeriaYAccesoriosDB.sql
@@ -23,8 +23,18 @@ use DB_PerfumeriaYAccesorios;
 
 
 -- 7. detalleVentas (Le toca a Pablo)
-
-
+create table DetalleVentas (
+    codigoDetalleVenta int auto_increment, 
+	cantidad int not null,
+    precioUnitario decimal(10,2)not null,
+    primary key PK_codigoDetalleVenta(codigoDetalleVenta),
+    codigoVenta int,
+    codigoProducto int,
+    constraint FK_detalleVenta_venta foreign key (codigoVenta) 
+		references Ventas(codigoVenta) on delete cascade,
+    constraint FK_detalleVenta_producto foreign key (codigoProducto)
+		references Productos(codigoProducto)on delete cascade
+);
 -- 8. facturas (Le toca a Iverson)
 
  -- 9. compras (Le toca a Iverson)
@@ -54,7 +64,59 @@ use DB_PerfumeriaYAccesorios;
 
 -- DETALLEVENTAS
 -- CRUD TRABAJADO POR Pablo
+delimiter $$
+create procedure sp_agregarDetalleVenta(
+    in cantidadDV int,
+    in precioUnitarioDV decimal(10,2),
+    in codigoVenta int,
+    in codigoProducto int)
+begin
+    insert into DetalleVentas (cantidad, precioUnitario, codigoVenta, codigoProducto)
+    values (cantidadDV, precioUnitarioDV, codigoVenta, codigoProducto);
+end $$
+delimiter ;
 
+delimiter $$
+create procedure sp_listarDetalleVentas()
+begin
+    select codigoDetalleVenta, cantidad, precioUnitario, codigoVenta, codigoProducto
+    from DetalleVentas;
+end $$
+delimiter ;
+
+delimiter $$
+create procedure sp_buscarDetalleVenta(in codigoDV int)
+begin
+    select codigoDetalleVenta, cantidad, precioUnitario, codigoVenta, codigoProducto
+    from DetalleVentas
+    where codigoDetalleVenta = codigoDV;
+end $$
+delimiter ;
+
+delimiter $$
+create procedure sp_editarDetalleVenta(
+    in codigoDV int,
+    in cantidadDV int,
+    in precioUnitarioDV decimal(10,2),
+    in codigoVenta int,
+    in codigoProducto int)
+begin
+    update DetalleVentas
+    set cantidad = cantidadDV,
+        precioUnitario = precioUnitarioDV,
+        codigoVenta = codigoVenta,
+        codigoProducto = codigoProducto
+    where codigoDetalleVenta = codigoDV;
+end $$
+delimiter ;
+
+delimiter $$
+create procedure sp_eliminarDetalleVenta(in codigoDV int)
+begin
+    delete from DetalleVentas
+		where codigoDetalleVenta = codigoDV;
+end $$
+delimiter ;
 -- Facturas
 -- CRUD TRABAJADO POR Iverson
 
