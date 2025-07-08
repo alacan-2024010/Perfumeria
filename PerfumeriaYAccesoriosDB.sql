f0a0e262552bcafdbb408ec1f0efe6e9c4748bce
aroquel-2024082 <aroquel-2024082@kinal.edu.gt>
2025-07-07 18:15:41 -0600

    FEAT: Agregar entidad y procedimientos almacenados de Empleados

--- PerfumeriaYAccesoriosDB.sql
+++ PerfumeriaYAccesoriosDB.sql
@@ -17,6 +17,16 @@ use DB_PerfumeriaYAccesorios;
 
 -- 5. empleados (Le toca a Angel)
 
+create table Empleados (
+	codigoEmpleado int auto_increment,
+	nombreEmpleado varchar(100) not null,
+	apellidoEmpleado varchar(100) not null,
+	direccionEmpleado varchar(255) not null,
+	telefonoEmpleado varchar(8) not null,
+	emailEmpleado varchar(100) not null,
+	puestoEmpleado varchar(50)not null,
+    primary key PK_idEmpleado(codigoEmpleado)
+);
 
 -- 6. ventas (Le toca a Cristian)
 
@@ -48,6 +58,83 @@ use DB_PerfumeriaYAccesorios;
 
 -- EMPLEADOS
 -- CRUD TRABAJADO POR Angel
+
+delimiter $$
+	create procedure sp_agregarEmpleado(
+		in nombreE varchar(100),
+		in apellidoE varchar(100),
+		in direccionE varchar(255),
+		in telefonoE varchar(8),
+		in emailE varchar(100),
+		in puestoE varchar(50))
+	begin
+		insert into empleados (nombreEmpleado, apellidoEmpleado, direccionEmpleado, telefonoEmpleado, emailEmpleado, puestoEmpleado)
+		values (nombreE, apellidoE, direccionE, telefonoE, emailE, puestoE);
+	end $$
+delimiter ;
+
+CALL sp_agregarEmpleado('Carlos', 'Méndez', 'Zona 7, Ciudad de Guatemala', '55671234', 'cmendez@empresa.com', 'Veterinario');
+CALL sp_agregarEmpleado('Ana', 'González', 'Zona 10, Ciudad de Guatemala', '51234567', 'agonzalez@empresa.com', 'Recepcionista');
+CALL sp_agregarEmpleado('Luis', 'Ramírez', 'Zona 5, Mixco', '55889977', 'lramirez@empresa.com', 'Administrador');
+CALL sp_agregarEmpleado('María', 'Pérez', 'Zona 11, Ciudad de Guatemala', '55443322', 'mperez@empresa.com', 'Asistente Veterinaria');
+CALL sp_agregarEmpleado('Jorge', 'Sánchez', 'Zona 18, Ciudad de Guatemala', '56781234', 'jsanchez@empresa.com', 'Veterinario');
+CALL sp_agregarEmpleado('Claudia', 'Herrera', 'Zona 1, Ciudad de Guatemala', '52346789', 'cherrera@empresa.com', 'Recepcionista');
+CALL sp_agregarEmpleado('Pedro', 'López', 'Zona 16, Ciudad de Guatemala', '57684321', 'plopez@empresa.com', 'Encargado de Inventario');
+CALL sp_agregarEmpleado('Sofía', 'Martínez', 'Villa Nueva, Guatemala', '58991123', 'smartinez@empresa.com', 'Veterinaria');
+CALL sp_agregarEmpleado('Ricardo', 'Ortega', 'Zona 2, Ciudad de Guatemala', '53456789', 'rortega@empresa.com', 'Gerente');
+CALL sp_agregarEmpleado('Andrea', 'Morales', 'Amatitlán, Guatemala', '51239876', 'amorales@empresa.com', 'Recepcionista');
+CALL sp_agregarEmpleado('Kevin', 'Ruiz', 'Zona 6, Ciudad de Guatemala', '56784321', 'kruiz@empresa.com', 'Veterinario');
+CALL sp_agregarEmpleado('Brenda', 'Castillo', 'Villa Canales, Guatemala', '53334444', 'bcastillo@empresa.com', 'Asistente Administrativa');
+CALL sp_agregarEmpleado('Oscar', 'Fernández', 'Zona 3, Mixco', '59001234', 'ofernandez@empresa.com', 'Encargado de Limpieza');
+CALL sp_agregarEmpleado('Elena', 'Vásquez', 'Zona 12, Ciudad de Guatemala', '57894561', 'evasquez@empresa.com', 'Veterinaria');
+CALL sp_agregarEmpleado('David', 'Cabrera', 'Santa Catarina Pinula, Guatemala', '52349876', 'dcabrera@empresa.com', 'Técnico de Laboratorio');
+
+delimiter $$
+	create procedure sp_listarEmpleados()
+	begin
+		select codigoEmpleado, nombreEmpleado, apellidoEmpleado, direccionEmpleado, telefonoEmpleado, emailEmpleado, puestoEmpleado
+		from empleados;
+	end $$
+delimiter ;
+
+delimiter $$
+	create procedure sp_buscarEmpleado(in codigoE int)
+	begin
+		select codigoEmpleado, nombreEmpleado, apellidoEmpleado, direccionEmpleado, telefonoEmpleado, emailEmpleado, puestoEmpleado
+		from empleados
+		where codigoEmpleado = codigoE;
+	end $$
+delimiter ;
+
+delimiter $$
+	create procedure sp_editarEmpleado(
+		in codigoE int,
+		in nombreE varchar(100),
+		in apellidoE varchar(100),
+		in direccionE varchar(255),
+		in telefonoE varchar(8),
+		in emailE varchar(100),
+		in puestoE varchar(50))
+	begin
+		update empleados
+		set nombreEmpleado = nombreE,
+			apellidoEmpleado = apellidoE,
+			direccionEmpleado = direccionE,
+			telefonoEmpleado = telefonoE,
+			emailEmpleado = emailE,
+			puestoEmpleado = puestoE
+		where codigoEmpleado = codigoE;
+	end $$
+delimiter ;
+
+delimiter $$
+create procedure sp_eliminarEmpleado(
+	in codigoE int)
+begin
+	delete from Empleados
+		Where codigoE = codigoEmpleado;
+end $$
+delimiter ;
 
 -- VENTAS
 -- CRUD TRABAJADO POR Cristian
