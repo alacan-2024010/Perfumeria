239f5dcc9f5a8cad9f69b928fe381fe8def5f4fb
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2025-07-07 18:05:52 -0600

    Feat:Agregar base de datos para que cada uno la edite

--- PerfumeriaYAccesoriosDB.sql
+++ PerfumeriaYAccesoriosDB.sql
@@ -3,30 +3,16 @@ create database DB_PerfumeriaYAccesorios;
 use DB_PerfumeriaYAccesorios;
 
 -- 1. clientes (Le toca a Alan)
-create table Clientes (
-	codigoCliente int not null auto_increment,
-	nombreCliente varchar(100) not null,
-	apellidoCliente varchar(100) not null,
-	direccionCliente varchar(255) not null,
-	telefonoCliente varchar(8) not null,
-	emailCliente varchar(100),
-    fechaCompra date,
-	primary key PK_codigoCliente(codigoCliente)
-);
 
 
 
 -- 2. proveedores (Le toca a Adrian)
 
 
-
 -- 3. categorias (Le toca a Chacón)
 
 
-
 -- 4. productos (Le toca a Chacón)
-
-
 
 
 -- 5. empleados (Le toca a Angel)
@@ -35,13 +21,45 @@ create table Clientes (
 -- 6. ventas (Le toca a Cristian)
 
 
+
 -- 7. detalleVentas (Le toca a Pablo)
 
 
 -- 8. facturas (Le toca a Iverson)
 
-
  -- 9. compras (Le toca a Iverson)
  
  
  -- 10. detalleCompras (Le toca a Cristopher)
+
+
+
+-- CLIENTES
+-- CRUD TRABAJADO POR Alan Lacán
+
+-- PROVEEDORES
+-- CRUD TRABAJADO POR Adrián
+
+-- CATEGORIAS
+-- CRUD TRABAJADO POR Chacón
+
+-- PRODUCTOS
+-- CRUD TRABAJADO POR Chacón
+
+-- EMPLEADOS
+-- CRUD TRABAJADO POR Angel
+
+-- VENTAS
+-- CRUD TRABAJADO POR Cristian
+
+-- DETALLEVENTAS
+-- CRUD TRABAJADO POR Pablo
+
+-- Facturas
+-- CRUD TRABAJADO POR Iverson
+
+-- Compras
+-- CRUD TRABAJADO POR Iverson
+
+-- DETALLECOMPRAS
+-- CRUD TRABAJADO POR Cristopher
