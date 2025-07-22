3af9e742e647e80d86da2305d7b0a28c839866f7
aroquel-2024082 <aroquel-2024082@kinal.edu.gt>
2025-07-21 21:57:35 -0600

    Fix: corregir el commit que se realizo anteriormente

---
+++ ProyectoPerfumeria/target/generated-sources/annotations/com/alanlacan/dominio/Categoria_.java
@@ -0,0 +1,14 @@
+package com.alanlacan.dominio;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-07-21T21:46:35", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Categoria.class)
+public class Categoria_ { 
+
+    public static volatile SingularAttribute<Categoria, Integer> codigoCategoria;
+    public static volatile SingularAttribute<Categoria, String> nombre;
+
+}
---
+++ ProyectoPerfumeria/target/generated-sources/annotations/com/alanlacan/dominio/Producto_.java
@@ -0,0 +1,22 @@
+package com.alanlacan.dominio;
+
+import com.alanlacan.dominio.Categoria;
+import com.alanlacan.dominio.Proveedor;
+import java.math.BigDecimal;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-07-21T21:46:35", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Producto.class)
+public class Producto_ { 
+
+    public static volatile SingularAttribute<Producto, String> descripcionProducto;
+    public static volatile SingularAttribute<Producto, Categoria> categoria;
+    public static volatile SingularAttribute<Producto, Proveedor> proveedor;
+    public static volatile SingularAttribute<Producto, Integer> codigoProducto;
+    public static volatile SingularAttribute<Producto, BigDecimal> precioProducto;
+    public static volatile SingularAttribute<Producto, Integer> stock;
+    public static volatile SingularAttribute<Producto, String> nombreProducto;
+
+}
